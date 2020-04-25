97c0a2989c939f070d6c5ae7b9b46e0d40a08748
Egor K <bo4ka21@gmail.com>
2020-04-25 20:16:45 +0300

    Added Highlight Of active router link

--- src/app/Router/Router.js
+++ src/app/Router/Router.js
@@ -1,31 +1,55 @@
 class Router {
   constructor(components) {
     this.components = components; // Receiving Components with Appropriate route
-
+    this.activeLink = null;
     this.workSpace = document.querySelector('.workspace');
+
+    // Path "/" to load default component
+    this.path = window.location.pathname;
+
     // Listeners to Load different components depending on its route
-    this.routerLink = document.querySelectorAll('.router-link');
-    this.routerLink.forEach((el) =>
-      el.addEventListener('click', this.linkEvent)
-    );
+    this.routerLinks = document.querySelectorAll('.router-link');
+    this.routerLinks.forEach((el) => {
+      const link = el.getAttribute('href');
+      if (link === this.path) {
+        this.activeLink = el;
+        this.activeLink.classList.add('active');
+      }
+      el.addEventListener('click', this.linkEvent);
+    });
 
     // When users steps back to previous route in history object
     // Load component depending on its route
     window.addEventListener('popstate', (e) => {
       let path = e.state || '/';
+
+      this.routerLinks.forEach((el) => {
+        if (el.getAttribute('href') === path) {
+          this.highlightActiveLink(el);
+        }
+      });
+
       this.renderComponent(path, 'freeze');
     });
 
-    // Path "/" to load default component
-    this.path = window.location.pathname;
     this.renderComponent(this.path, 'freeze');
   }
 
   linkEvent = (e) => {
     e.preventDefault();
+    console.log(e);
     this.path = e.target.getAttribute('href');
+
+    this.highlightActiveLink(e.target);
+
     this.renderComponent(this.path, 'push');
   };
+
+  highlightActiveLink(currentLink) {
+    this.activeLink.classList.remove('active');
+    currentLink.classList.add('active');
+    this.activeLink = currentLink;
+  }
 
   renderComponent(path, action = 'push') {
     // @Screen@ variable stores necessary component and then passes it
@@ -35,7 +59,6 @@ class Router {
     for (let element of components) {
       if (element.path === path) Screen = new element.component();
     }
-    console.log(Screen);
     let screenContent = Screen.getMarkup();
     workSpace.removeChild(workSpace.childNodes[0]);
     workSpace.appendChild(screenContent);
